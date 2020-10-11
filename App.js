0a629418275e9f03a5393aa77f0d98ca97a0c4ef
Glebova Anna <35535292+ritchann@users.noreply.github.com>
2020-10-11 23:00:59 +0300

    add_todo_item

--- App.js
+++ App.js
@@ -1,15 +1,33 @@
 import { StatusBar } from "expo-status-bar";
-import React from "react";
-import { StyleSheet, Text, View } from "react-native";
+import React, { useState } from "react";
+import { FlatList, ScrollView, StyleSheet, Text, View } from "react-native";
 import { AddTodo } from "./src/AddTodo";
 import { Navbar } from "./src/Navbar";
+import { Todo } from "./src/Todo";
 
 export default function App() {
+  const [todos, setTodos] = useState([]);
+
+  const addTodo = (title) => {
+    setTodos((prev) => [
+      ...prev,
+      {
+        id: Date.now().toString(),
+        title,
+      },
+    ]);
+  };
+
   return (
-    <View >
+    <View>
       <Navbar title="Todo App" />
       <View style={styles.container}>
-        <AddTodo />
+        <AddTodo onSubmit={addTodo} />
+        <FlatList
+          keyExtractor={(item) => item.id.toString()}
+          data={todos}
+          renderItem={({ item }) => <Todo todo={item} />}
+        />
       </View>
     </View>
   );
--- src/AddTodo.js
+++ src/AddTodo.js
@@ -1,26 +1,43 @@
 import { StatusBar } from "expo-status-bar";
-import React from "react";
-import { Button, StyleSheet, Text, TextInput, View } from "react-native";
+import React, { useState } from "react";
+import { Alert, Button, StyleSheet, Text, TextInput, View } from "react-native";
 
-export const AddTodo = ({ title }) => {
+export const AddTodo = ({ onSubmit }) => {
+  const [value, setValue] = useState("");
+
+  const pressHandler = () => {
+    if (value.trim()) {
+      onSubmit(value);
+      setValue("");
+    } else Alert.alert("Название не может быть пустым");
+  };
+
   return (
     <View style={styles.block}>
-        <TextInput style={styles.input}></TextInput>
-        <Button  title='Добавить'/>
+      <TextInput
+        placeholder="Введите название..."
+        value={value}
+        onChangeText={setValue}
+        style={styles.input}
+        autoCorrect={false}
+        autoCapitalize="none"
+      ></TextInput>
+      <Button title="Добавить" onPress={pressHandler} />
     </View>
   );
 };
 
 const styles = StyleSheet.create({
-  block:{
-      flexDirection:'row',
-      justifyContent:'space-between',
-      alignItems:'center'
+  block: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+    alignItems: "center",
+    marginBottom: 1,
   },
-  input:{
-      width:'70%',
-      borderStyle:'solid',
-      borderBottomWidth:2,
-      borderBottomColor:'#3949ab'
-  }
+  input: {
+    width: "70%",
+    borderStyle: "solid",
+    borderBottomWidth: 2,
+    borderBottomColor: "#3949ab",
+  },
 });
